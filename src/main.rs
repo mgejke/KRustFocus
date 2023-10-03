05fad63c8f2f8e191832a164ecff373c4af25464
Martin Gejke <martin.gejke@acorntechnology.se>
2023-10-03 16:27:31 +0200

    Add tray icon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem = "windows"]
+
 mod handlers;
 mod models;
 
@@ -7,12 +9,21 @@ use models::ControlFlow;
 use windows_hotkeys::keys::{ModKey, VKey};
 use windows_hotkeys::{HotkeyManager, HotkeyManagerImpl};
 
+use tray_item::{IconSource, TrayItem};
+
 fn main() -> Result<()> {
     let mut hkm = HotkeyManager::new();
-
     hkm.register(VKey::Left, &[ModKey::Ctrl, ModKey::Alt], left)?;
     hkm.register(VKey::Right, &[ModKey::Ctrl, ModKey::Alt], right)?;
     hkm.register(VKey::L, &[ModKey::Ctrl, ModKey::Alt, ModKey::Shift], quit)?;
+    let handle = hkm.interrupt_handle();
+
+    let mut tray = TrayItem::new("Tray Example", IconSource::Resource("focus_tray")).unwrap();
+    tray.add_label("Window focuser")?;
+    tray.inner_mut().add_separator()?;
+    tray.add_menu_item("Quit", move || {
+        handle.interrupt();
+    })?;
 
     loop {
         match hkm.handle_hotkey() {
